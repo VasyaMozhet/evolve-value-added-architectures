b95cedcb56c3c86b25f17404a2ca6c2e661e0421
VasyaMozhet <158567931+VasyaMozhet@users.noreply.github.com>
2024-04-15 06:57:08 +0200

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,16 @@
+    const data = [];
+    });
+main();
+
+    data.forEach(item => {
+
+    }
+}
+    for (let i = 0; i < 10; i++) {
+    return data;
+        console.log("Random Number: " + item);
+        data.push(Math.floor(Math.random() * 100) + 1);
+function generateRandomData() {
+    const data = generateRandomData();
+function main() {
+}
